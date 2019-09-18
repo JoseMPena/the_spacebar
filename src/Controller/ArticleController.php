240082b07fed063e7f1bbe268e7394fa0116f0da
Jose M Peña <josmpena91@gmail.com>
2019-09-18 17:42:40 +0200

    profiling, debuging and unpacking

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -24,6 +24,7 @@ class ArticleController extends AbstractController
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
+
       return $this->render('article/show.html.twig', [
         'title' => \ucwords(\str_replace('-', ' ', $article_slug)),
         'comments' => $comments
