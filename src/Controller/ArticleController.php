c6d9dc1ffd159b41b57582cb9ae5fc04c263590c
Jose M Peña <josmpena91@gmail.com>
2019-09-18 17:20:37 +0200

    Intro to twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,9 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -18,6 +19,14 @@ class ArticleController
     */
     public function show($article_slug)
     {
-      return new Response(sprintf('Future article: "%s"', $article_slug));
+      $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+      return $this->render('article/show.html.twig', [
+        'title' => \ucwords(\str_replace('-', ' ', $article_slug)),
+        'comments' => $comments
+      ]);
     }
 }
